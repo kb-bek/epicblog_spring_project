ae850a47053f21bd7db5f5370a04a44def8a37cd
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-06-12 21:47:06 +0600

    Added InvalidLoginResponse for error message

---
+++ src/main/java/com/example/epicblog/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,13 @@
+package com.example.epicblog.payload.response;
+
+public class InvalidLoginResponse {
+
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+    }
+
+}

79330697ec02cf035bedd6e23871cd46e83b43aa
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-06-12 19:31:20 +0600

    Started developing pet prjoject - epicblog: software for sharing blog

---
+++ src/main/java/com/example/epicblog/EpicBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.epicblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicBlogApplication.class, args);
+	}
+
+}

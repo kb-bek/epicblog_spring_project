48fa0c791d93c38bd4037150f8c24671ba2a5522
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-06-12 19:35:24 +0600

    Added repositories for models

---
+++ src/main/java/com/example/epicblog/repository/CommentRepository.java
@@ -0,0 +1,17 @@
+package com.example.epicblog.repository;
+
+import com.example.epicblog.entity.Comment;
+import com.example.epicblog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(Long commentId, Long userId);
+
+}
